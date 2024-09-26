d534c616bc0af05fa10c661aa493b255bff567a3
Malik-Rizwan0 <malikrizwan1076@gmail.com>
2024-09-26 19:28:43 +0500

    Add Store

--- BLOG WEBSITE (MERN)/blog website/client/src/Component/Posts/Posts.jsx
+++ BLOG WEBSITE (MERN)/blog website/client/src/Component/Posts/Posts.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import './Posts.css'
 import Post from '../Post/Post'
-function Posts() {
+function Posts(posts) {
     return (
 
-        <div className='posts'>
-            <Post/>
-            <Post/>
-            <Post/>
-            <Post/>
-            <Post/>
+        <div className='posts' >
+             {
+                posts.map((p, index) => (
+                    <Post key={index} {...p} />  // Pass each post as props to Post component
+                ))
+            }
         </div>
     )
 }
--- BLOG WEBSITE (MERN)/blog website/client/src/Home Page/Home.jsx
+++ BLOG WEBSITE (MERN)/blog website/client/src/Home Page/Home.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
-import './home.css'
-import Header from '../Component/header/Header'
-import Sidebar from '../Component/Sidebar/Sidebar'
-import Posts from '../Component/Posts/Posts'
+import React from 'react';
+import './home.css';
+import Header from '../Component/header/Header';
+import Sidebar from '../Component/Sidebar/Sidebar';
+import Posts from '../Component/Posts/Posts';
+import axios from 'axios'
+import { useState } from 'react';
+import { useEffect } from 'react';
+
 
 function Home() {
+  const [posts , setPosts] = useState([]);
+  useEffect(() => {
+    const fetchPosts = async ()=>{
+      const res = await axios.get("/api/posts")
+      setPosts(res.data)
+    }
+    fetchPosts()
+    
+  }, [])
+  
   return (
     <>
       <Header />
       <div className="home">
-        <Posts/>
+        <Posts posts={posts}/>
         <Sidebar />
       </div>
     </>
